f669692a484952964a75c7bdd092664110cdd07d
tar0 <kamimura@starfish.co.jp>
2023-11-15 16:00:41 +0900

    Add phone number placeholder and improve result display

--- carrier_check/script.js
+++ carrier_check/script.js
@@ -1,15 +1,24 @@
 document.getElementById('phoneForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
+    // 結果表示エリアを取得し、前回の結果をクリア
+    var resultDiv = document.getElementById('result');
+    resultDiv.innerText = '調査中.........';
+
+    // 電話番号を取得
     var phone = document.getElementById('phone').value;
     var url = 'https://us-central1-private-287112.cloudfunctions.net/promotion_check_carrier_name?phone_number=' + encodeURIComponent(phone);
 
+    // APIリクエストを送信
     fetch(url)
-        .then(response => response.text()) // JSONではなくテキストとしてレスポンスを解析
+        .then(response => response.text())
         .then(text => {
-            document.getElementById('result').innerText = 'キャリア: ' + text; // テキストデータを使用
+            // テキストデータを使用して結果を表示
+            resultDiv.innerText = 'キャリア: ' + text;
         })
         .catch(error => {
+            // エラーをキャッチし、結果表示エリアにエラーメッセージを表示
             console.error('エラー:', error);
+            resultDiv.innerText = 'その番号は存在しません';
         });
 });
